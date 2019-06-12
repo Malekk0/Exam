3b2d52de59dde796b4dfa1a846d90fce4fb5d907
GKenar <kenar.gf@gmail.com>
2019-06-12 15:02:17 +0300

    Добавлен LinkedList

---
+++ Заготовки/LinkedList/LinkedList/LList.cs
@@ -0,0 +1,60 @@
+﻿using System.Text;
+
+namespace LinkedList
+{
+    public class LList<T>
+    {
+        private class Node
+        {
+            public T value;
+            public Node next;
+        }
+
+        private Node _head;
+
+        public void Enqueue(T val)
+        {
+            if (_head == null)
+            {
+                _head = new Node { value = val };
+                return;
+            }
+
+            var temp = _head;
+            while (temp.next != null)
+            {
+                temp = temp.next;
+            }
+            temp.next = new Node { value = val };
+        }
+
+        public void Push(T val)
+        {
+            if (_head == null)
+            {
+                _head = new Node { value = val };
+                return;
+            }
+
+            var temp = _head;
+            _head = new Node { value = val, next = temp };
+        }
+
+        public override string ToString()
+        {
+            var str = new StringBuilder();
+            PrintToStringBuilder(_head, str, ", ");
+
+            return str.ToString();
+        }
+
+        private void PrintToStringBuilder(Node currentNode, StringBuilder str, string divider)
+        {
+            if (currentNode == null)
+                return;
+
+            str.Append(currentNode.value + divider);
+            PrintToStringBuilder(currentNode.next, str, divider);
+        }
+    }
+}
---
+++ Заготовки/LinkedList/LinkedList/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LList<double> list = new LList<double>();
+
+            list.Push(-11.5);
+            list.Enqueue(1);
+            list.Enqueue(5);
+            list.Enqueue(-2);
+            list.Push(55.8);
+            list.Enqueue(0);
+            list.Enqueue(10);
+            list.Push(12);
+
+            Console.WriteLine(list.ToString());
+        }
+    }
+}
